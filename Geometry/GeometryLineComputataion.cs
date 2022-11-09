8ef44a7c9543caaa8da5b50eb461db43dba132fe
Vinay <navik.vinay12@gmail.com>
2022-11-09 18:06:02 +0530

    welcome msg

---
+++ Geometry/GeometryLineComputataion.cs
@@ -0,0 +1,10 @@
+﻿namespace Geometry
+{
+    internal class GeometryLineComputataion
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+        }
+    }
+}
